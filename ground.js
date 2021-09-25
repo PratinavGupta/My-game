580d32290075ef9e35e91f0c8e483a949ae8a085
PratinavGupta <28yashicagupta@gmail.com>
2021-09-25 18:09:14 +0530

    error in first screen button

---
+++ ground.js
@@ -0,0 +1,21 @@
+// class GROUND {
+
+
+//     constructor(x, y, w, h) {
+//         var prop = { isStatic: true }
+//         this.body = Matter.Bodies.rectangle(x, y, w, h, prop)
+//         Matter.World.add(world, this.body)
+//         this.w = w
+//         this.h = h
+//     }
+//     display() {
+//         rectMode(CENTER)
+//         rect(this.body.position.x, this.body.position.y, this.w, this.h)
+
+//     }
+
+
+
+
+
+// }
